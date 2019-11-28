b70b2e46f0bb4b73586211543bb97a65ef63468f
David GERARD <david.gerard@telecom-paris.fr>
2019-11-28 19:26:33 +0100

    adding code to make the light circle around the LED strip

--- LED_Phare/LED_Phare.ino
+++ LED_Phare/LED_Phare.ino
@@ -1,17 +1,35 @@
+//Arduino code to control the lighthouse's LEDs
+//This Code was made by David GERARD, member of the 2019-2020 Telecom Paris French robotic cup team.
+
 #include <APA102.h>
 const uint8_t dataPin = 11;
 const uint8_t clockPin = 12;
 APA102<dataPin, clockPin> ledStrip;
 
-const uint16_t ledCount = 30;
+const uint16_t ledCount = 30; // number of LEDs in the strip
 rgb_color colors[ledCount];
+int i = 1;
+
+
 
 void setup() {
-  
+   
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+        
+        colors[(i-1)%ledCount].red = 0;
+        colors[(i-1)%ledCount].green = 0;
+        colors[(i-1)%ledCount].blue = 0;
+        
+        colors[i%ledCount].red = 225;
+        colors[i%ledCount].green = 255;
+        colors[i%ledCount].blue = 225;
 
+        i++;
+
+        ledStrip.write(colors, ledCount, 31);
+        delay(100);
 }
