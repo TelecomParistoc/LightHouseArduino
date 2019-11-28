fefefa1fe3f21c8910da45520310dc0dfecdca75
David GERARD <david.gerard@telecom-paris.fr>
2019-11-28 18:52:27 +0100

    ajout du fichier LED

---
+++ LED_Phare/LED_Phare.ino
@@ -0,0 +1,17 @@
+#include <APA102.h>
+const uint8_t dataPin = 11;
+const uint8_t clockPin = 12;
+APA102<dataPin, clockPin> ledStrip;
+
+const uint16_t ledCount = 30;
+rgb_color colors[ledCount];
+
+void setup() {
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
