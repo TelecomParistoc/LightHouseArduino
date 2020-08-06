6a26fcfe0ef296163bb3ace4b85fd2e06bd0dd8a
Nima Alikhani <nimajaune@gmail.com>
2020-08-06 15:44:03 +0200

    typo + gitignore for macos

--- LED_Phare/LED_Phare.ino
+++ LED_Phare/LED_Phare.ino
@@ -2,6 +2,7 @@
 //This Code was made by David GERARD, member of the 2019-2020 Telecom Paris French robotic cup team.
 
 #include <APA102.h>
+
 const uint8_t dataPin = 11;
 const uint8_t clockPin = 12;
 APA102<dataPin, clockPin> ledStrip;
@@ -10,16 +11,11 @@ const uint16_t ledCount = 30; // number of LEDs in the strip
 rgb_color colors[ledCount];
 int i = 1;
 
-
-
-void setup() {
-   
-
+void setup() {   
 }
 
 void loop() 
-{
-        
+{   
         colors[(i-1)%ledCount].red = 0;
         colors[(i-1)%ledCount].green = 0;
         colors[(i-1)%ledCount].blue = 0;
